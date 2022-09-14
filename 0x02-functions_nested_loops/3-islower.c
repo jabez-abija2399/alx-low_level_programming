af6733ef5db90af790dbc61f7fc10a0d2822837d
jabez-abija2399 <jabeznew2399@gmail.com>
2022-09-14 08:35:11 -0700

    all

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,11 @@
 /*
  * File: 3-islower.c
- * Auth: yabets 
+ * Auth: yabets
  */
-
 #include "main.h"
-
 /**
  * _islower - Checks if a character is lowercase.
  * @c: The character to be checked.
- *
  * Return: 1 if character is lowercase, 0 otherwise.
  */
 int _islower(int c)
