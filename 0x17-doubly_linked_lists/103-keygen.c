0aa8a5340fbb126cd29672033f196ad7f004687a
jabez-abija2399 <jabeznew2399@gmail.com>
2022-12-08 08:26:08 -0800

    jsdhf

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -2,9 +2,11 @@
  * File: 103-keygen.c
  * Auth: yabets
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * main - Generates and prints passwords for the crackme5 executable.
  * @argc: The number of arguments supplied to the program.
@@ -16,17 +18,22 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	char password[7], *codex;
 	int len = strlen(argv[1]), i, tmp;
+
 	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
 	tmp = (len ^ 59) & 63;
 	password[0] = codex[tmp];
+
 	tmp = 0;
 	for (i = 0; i < len; i++)
 		tmp += argv[1][i];
 	password[1] = codex[(tmp ^ 79) & 63];
+
 	tmp = 1;
 	for (i = 0; i < len; i++)
 		tmp *= argv[1][i];
 	password[2] = codex[(tmp ^ 85) & 63];
+
 	tmp = 0;
 	for (i = 0; i < len; i++)
 	{
@@ -40,9 +47,11 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	for (i = 0; i < len; i++)
 		tmp += (argv[1][i] * argv[1][i]);
 	password[4] = codex[(tmp ^ 239) & 63];
+
 	for (i = 0; i < argv[1][0]; i++)
 		tmp = rand();
 	password[5] = codex[(tmp ^ 229) & 63];
+
 	password[6] = '\0';
 	printf("%s", password);
 	return (0);
